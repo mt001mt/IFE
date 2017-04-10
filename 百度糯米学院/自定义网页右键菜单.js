4e825e1697af5b009aadae67650cc1abd9f002f9
MT <mt001mt@163.com>
2017-04-10 11:29:47 +0800

    place css style in html

--- 百度糯米学院/自定义网页右键菜单.js
+++ 百度糯米学院/自定义网页右键菜单.js
@@ -2,12 +2,12 @@
 
 $(window).ready(function() {
   //右键点击div，出现自定义菜单栏事件
-  $("div").contextmenu(function() {
-    event.preventDefault();
-    event.stopPropagation();
-    let menu = $(".user-menu"),
-        left = event.pageX,
-        top = event.pageY,
+  $("div").contextmenu(function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    var menu = $(".user-menu"),
+        left = e.pageX,
+        top = e.pageY,
         width = menu.outerWidth(),
         height = menu.outerHeight(),
         innerWidth = $(window).width(),
@@ -25,28 +25,28 @@ $(window).ready(function() {
     $(".user-menu").show();
   });
   //左键点击事件
-  $(document).click(function() {
-    let menu = $(".user-menu");
+  $(document).click(function(e) {
+    var menu = $(".user-menu");
     ////自定义菜单栏隐藏
     if (menu.css("display") === "block") {
       menu.hide();
     }
     ////执行自定义菜单栏的命令
-    if (event.target === menu.find(".command-1")[0]) {
+    if (e.target === menu.find(".command-1")[0]) {
       setTimeout(function() {
         alert("显示一个对话框");
       }, 50);
     }
   });
   //右键点击事件
-  $(document).contextmenu(function() {
-    let menu = $(".user-menu");
+  $(document).contextmenu(function(e) {
+    var menu = $(".user-menu");
     ////自定义菜单栏隐藏
     if (menu.css("display") === "block") {
       menu.hide();
     }
-    if ($.contains(menu[0], event.target)) {
-      event.preventDefault();
+    if ($.contains(menu[0], e.target)) {
+      e.preventDefault();
     } 
   });
 });
